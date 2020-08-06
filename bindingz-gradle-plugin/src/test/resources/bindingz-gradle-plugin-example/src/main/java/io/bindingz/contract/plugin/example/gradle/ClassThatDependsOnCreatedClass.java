275166bb97dd8e6578b7a0e036828ae4efe8825e
Connor Goulding <connorg_2000@hotmail.com>
2020-08-07 00:33:21 +0100

    Add a class that depends on a generated class

---
+++ bindingz-gradle-plugin/src/test/resources/bindingz-gradle-plugin-example/src/main/java/io/bindingz/contract/plugin/example/gradle/ClassThatDependsOnCreatedClass.java
@@ -0,0 +1,7 @@
+package io.bindingz.contract.plugin.example.gradle;
+
+import io.bindingz.sample.java.LocalFooBarEvent;
+
+public class ClassThatDependsOnCreatedClass {
+    private LocalFooBarEvent localFooBarEvent;
+}

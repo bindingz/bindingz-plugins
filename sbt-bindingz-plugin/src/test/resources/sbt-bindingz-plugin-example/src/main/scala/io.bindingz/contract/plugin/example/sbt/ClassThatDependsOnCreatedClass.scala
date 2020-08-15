1f05a1a5cedeb9b22a7b1d3c6a4c0323b0bb9b7b
Connor Goulding <connorg_2000@hotmail.com>
2020-08-15 12:44:02 +0100

    Update plugin example

--- sbt-bindingz-plugin/src/test/resources/sbt-bindingz-plugin-example/src/main/scala/io.bindingz/contract/plugin/example/sbt/ClassThatDependsOnCreatedClass.scala
+++ sbt-bindingz-plugin/src/test/resources/sbt-bindingz-plugin-example/src/main/scala/io.bindingz/contract/plugin/example/sbt/ClassThatDependsOnCreatedClass.scala
@@ -3,5 +3,6 @@ package io.bindingz.contract.plugin.example.sbt
 import io.bindingz.sample.latest.InvoiceItemDto
 
 class ClassThatDependsOnCreatedClass {
-  var invoiceItem: InvoiceItemDto = null
+  var invoiceItem: InvoiceItemDto = new InvoiceItemDto()
+  invoiceItem.setOne("One")
 }
